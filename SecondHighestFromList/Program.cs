8baa1cd40bb001ea638369d8f141921494326f05
vishal.Alok <you@example.com>
2021-10-15 04:04:06 +0530

    Add project files.

---
+++ SecondHighestFromList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecondHighestFromList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> MyList = new List<int>() { 12,23,54};
+            MyList.Add(11); MyList.Add(11);
+            MyList.Add(5); MyList.Add(2);
+            MyList.Add(4); MyList.Add(6);
+
+            int largest = int.MinValue;
+            int second = int.MinValue;
+
+            foreach (int i in MyList)
+            {
+                if (i > largest)
+                {
+                    second = largest;
+                    largest = i;
+                }
+                else if (i > second)
+                    second = i;
+            }
+
+            Console.WriteLine(second);
+        }
+    }
+}
